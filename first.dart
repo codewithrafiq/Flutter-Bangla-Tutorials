c42ecc2b3ddf67e9c78bbc41da655913dcb249da
codewithrafiq <codewithrafiq@gmail.com>
2021-01-09 15:25:25 +0600

    part-1

---
+++ first.dart
@@ -0,0 +1,87 @@
+void main() {
+  // String = '' / ""
+  // Int = 454
+  // double = 4.565
+  // dynamic
+  // List
+  // bool
+  // Map
+
+  // dynamic name;
+  // name = 'CodeWithraiq';
+  // name = 4355;
+  // print(name);
+
+  // String name;
+  // name = "This is a String";
+  // print(name);
+
+  // int year;
+  // year = 44;
+  // print(year);
+
+  // List alist;
+  // String name = "Rafiq";
+  // List blist = [1, 2, 3, 4, 5];
+  // alist = ['string', 5454, 454.545, name, blist];
+  // print(alist[4][1]);
+
+  // bool t = true;
+  // bool f = false;
+
+  // Map mapdata = {
+  //   "name": 'CodeWithRafiq',
+  //   'age': 21,
+  //   'city': 'Comilla',
+  //   'country': 'Bangladesh',
+  //   'subject': ['Bangla', 'Math', 'DataScience', 'computer'],
+  //   'map2': {'a': 1, 'b': 2},
+  // };
+  // print(mapdata["subject"][0]);
+
+  // print(2 + 1);
+  // print(3 - 1);
+  // print(10 / 2);
+  // print(10 % 3);
+
+  // print(4 > 3);
+  // print(4 < 3);
+  // int num1 = 10;
+  // int num2 = 22;
+  // print(num1 > num2);
+  // print(num1 == num2);
+
+  // int numberr = 1;
+  // numberr -- ;
+
+  // print(2 * 4);
+
+  // num num1 = 5;
+  // num num2 = 455;
+  // print(num1 <= num2);
+
+  // num num1 = 6;
+  // num num2 = 55;
+
+  // if (num1<= num2) {
+  //   print("True");
+  // } else {
+  //   print("False");
+  // }
+
+  // num num1 = 6;
+  // num num2 = 55;
+  // if ((num1 != num2) && (num1 > num2)) {
+  //   print("You are Right");
+  // } else {
+  //   print("Wrong");
+  // }
+
+  num num1 = 6;
+  num num2 = 55;
+  if ((num1 != num2) || (num1 > num2)) {
+    print("You are Right");
+  } else {
+    print("Wrong");
+  }
+}
